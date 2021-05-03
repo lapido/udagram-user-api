bf87a343e9df4c219e596a08733d577bcf276516
Victor.Abidoye <victortemitope95@gmail.com>
2021-05-03 14:13:26 +0100

    update

--- src/server.ts
+++ src/server.ts
@@ -31,6 +31,9 @@ import { V0MODELS } from './controllers/v0/model.index';
     res.send( "/api/v0/" );
   } );
   
+  app.get('/health', async (req, res ) => {    
+    res.send(`up`);
+  });
 
   // Start the Server
   app.listen( port, () => {
